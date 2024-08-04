f22629bbfb0c7b913107d19348d81097fc81dc2a
LucasFagundesWielewski <lucas.fwielewski@gmail.com>
2024-08-03 23:58:33 -0300

    project cretion

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(new PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget{
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Olá Flutter'),
+    );
+  }
+}
