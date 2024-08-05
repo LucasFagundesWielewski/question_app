895618dc8a0d2189ca7151f9c549b41d1b7254f6
LucasFagundesWielewski <lucas.fwielewski@gmail.com>
2024-08-04 22:20:18 -0300

    added new questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new PerguntaApp());
-}
+main() => runApp(PerguntaApp());
+
 
 class PerguntaApp extends StatelessWidget{
 
+  void responder() {
+    print('Pergunta respondida!');
+  }
+  
+  void Function() responder2() {
+    return () => print('Pergunta respondida #2!');
+  }
+  @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
-      home: Text('Olá Flutter'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column( 
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder2(),
+            ),
+          ],
+        ),
+      ),
     );
   }
+  
+  RaisedButton({required Text child, required void Function() onPressed}) {}
 }
