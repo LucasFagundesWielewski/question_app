f51fb1390c9513c0b81b2890e49225476385ef23
LucasFagundesWielewski <lucas.fwielewski@gmail.com>
2024-08-07 20:29:14 -0300

    added questao.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'questao.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -26,7 +27,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas[_perguntaSelecionada]),
+            Questao(perguntas[_perguntaSelecionada]),
             ElevatedButton(
               child: Text('Resposta 1'),
               onPressed: _responder,
